4f464a5140f1da60e77a4e06451cedc837237e7b
chaoc <1197190972@qq.com>
2021-11-18 23:08:20 +0800

    test01 file path motifed

--- test01.py
+++ test01.py
@@ -59,14 +59,6 @@ def hook_fn_forward(module, input, output):
         plt.show()
         plt.close()
         pass
-        # for i, filters in enumerate(layer_viz):
-        #     if i == 16:
-        #         break
-        #     plt.subplot(2, 8, i + 1)
-        #     plt.imshow(filters)
-        #     plt.axis("off")
-        # plt.show()
-        # plt.close()
     # print(module)
     # print('input', input)
     # print('output', output)
@@ -86,23 +78,16 @@ def main():
     model = create_model(num_classes=7)
 
     # load train weights
-    train_weights = "/home/chaoc/Desktop/deep-learning-for-image-processing/pytorch_object_detection/faster_rcnn/save_weights/resNetFpn-model-19.pth"
+    train_weights = "/home/chaoc/Desktop/save_weights/resNetFpn-model-19.pth"
     assert os.path.exists(train_weights), "{} file dose not exist.".format(train_weights)
     model.load_state_dict(torch.load(train_weights, map_location=device)["model"])
     model.to(device)
 
-    # read class_indict
-    label_json_path = '/home/chaoc/Desktop/deep-learning-for-image-processing/pytorch_object_detection/faster_rcnn/pascal_voc_classes.json'
-    assert os.path.exists(label_json_path), "json file {} dose not exist.".format(label_json_path)
-    json_file = open(label_json_path, 'r')
-    class_dict = json.load(json_file)
-    json_file.close()
-    category_index = {v: k for k, v in class_dict.items()}
-    annotations_root = "/home/chaoc/Desktop/deep-learning-for-image-processing/data_set/test/IMAGES"
+    annotations_root = "/home/chaoc/Desktop/data_set/test/IMAGES"
     for i in os.listdir(annotations_root):
         # load image
-        original_img = Image.open("/home/chaoc/Desktop/deep-learning-for-image-processing/data_set/test/IMAGES/{}".format(i))
-        # original_img = Image.open("/home/chaoc/Desktop/macPycharm/hymenoptera_data/train/bees/39747887_42df2855ee.jpg")
+        original_img = Image.open("/home/chaoc/Desktop/data_set/test/IMAGES/{}".format(i))
+        # original_img = Image.open("/home/chaoc/Desktop/hymenoptera_data/train/bees/39747887_42df2855ee.jpg")
         # from pil image to tensor, do not normalize image
         data_transform = transforms.Compose([transforms.ToTensor()])
         img = data_transform(original_img)
@@ -115,37 +100,6 @@ def main():
         for name, module in modules:
             module.register_forward_hook(hook_fn_forward)
         o = model(img.to(device))
-
-        with torch.no_grad():
-            # init
-            img_height, img_width = img.shape[-2:]
-            init_img = torch.zeros((1, 3, img_height, img_width), device=device)
-            model(init_img)
-
-            t_start = time_synchronized()
-            predictions = model(img.to(device))[0]
-            t_end = time_synchronized()
-            print("inference+NMS time: {}".format(t_end - t_start))
-
-            predict_boxes = predictions["boxes"].to("cpu").numpy()
-            predict_classes = predictions["labels"].to("cpu").numpy()
-            predict_scores = predictions["scores"].to("cpu").numpy()
-
-            if len(predict_boxes) == 0:
-                print("没有检测到任何目标!")
-
-            draw_box(original_img,
-                     predict_boxes,
-                     predict_classes,
-                     predict_scores,
-                     category_index,
-                     thresh=0.06,
-                     line_thickness=3)
-            # 保存预测的图片结果
-            original_img.save(
-                "/home/chaoc/Desktop/deep-learning-for-image-processing/pytorch_object_detection/faster_rcnn/result/test_result{}.jpg".format(
-                    i))
-
 
 if __name__ == '__main__':
     main()
